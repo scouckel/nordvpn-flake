c5bf189a1a7ec73a489050c9d3e7d6a64afa1737
scouckel <james.krinsky@gmail.com>
2025-12-09 23:13:14 -0600

    module fix?

--- module.nix
+++ module.nix
@@ -21,44 +21,40 @@ with lib; {
     };
   };
 
-  config = [
-    mkIf config.services.nordvpn.enable {
-      environment.systemPackages = [ nordVPNPkgs ];
+  config = mkIf config.services.nordvpn.enable {
+    environment.systemPackages = [ nordVPNPkgs ];
 
-      users.groups.nordvpn = { };
+    users.groups.nordvpn = { };
 
-      systemd = {
-        services.nordvpn = {
-          description = "NordVPN daemon";
-          serviceConfig = {
-            ExecStart = "${nordVPNPkg}/bin/nordvpnd";
-            ExecStartPre = pkgs.writeShellScript "nordvpn-start" ''
-              mkdir -m 700 -p /var/lib/nordvpn;
-              if [ -z "$(ls -A /var/lib/nordvpn)" ]; then
-                cp -r ${nordVPNPkg}/var/lib/nordvpn/* /var/lib/nordvpn;
-              fi
-            '';
-            NonBlocking = true;
-            KillMode = "process";
-            Restart = "on-failure";
-            RestartSec = 5;
-            RuntimeDirectory = "nordvpn";
-            RuntimeDirectoryNode = "0750";
-            Group = "nordvpn";
-          };
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network-online.target" ];
-          wants = [ "network-online.target" ];
+    systemd = {
+      services.nordvpn = {
+        description = "NordVPN daemon";
+        serviceConfig = {
+          ExecStart = "${nordVPNPkg}/bin/nordvpnd";
+          ExecStartPre = pkgs.writeShellScript "nordvpn-start" ''
+            mkdir -m 700 -p /var/lib/nordvpn;
+            if [ -z "$(ls -A /var/lib/nordvpn)" ]; then
+              cp -r ${nordVPNPkg}/var/lib/nordvpn/* /var/lib/nordvpn;
+            fi
+          '';
+          NonBlocking = true;
+          KillMode = "process";
+          Restart = "on-failure";
+          RestartSec = 5;
+          RuntimeDirectory = "nordvpn";
+          RuntimeDirectoryNode = "0750";
+          Group = "nordvpn";
         };
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
       };
-    }
+    };
 
-    mkIf (config.services.nordvpn.enable && config.services.nordvpn.firewallChanges) {
-      networking.firewall = {
-        checkReversePath = false;
-        allowedTCPPorts = [443];
-        allowedUDPPorts = [1194];
-      };
-    }
-  ];
+    networking.firewall = if config.services.nordvpn.firewallChanges then {
+      checkReversePath = false;
+      allowedTCPPorts = [443];
+      allowedUDPPorts = [1194];
+    } else {};
+  };
 }
